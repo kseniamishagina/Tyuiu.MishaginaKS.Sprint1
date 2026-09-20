1811b404ac3715e8c061b1201d78901d6ca64f3f
MishaginaKS <kseniamishagina@mail.ru>
2026-09-20 19:40:52 +0500

    Add project files.

---
+++ Tyuiu.MishaginaKS.Sprint1.Task0.V5.Test/DataServuceTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MishaginaKS.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.MishaginaKS.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(12, res);
+        }
+    }
+}
---
+++ Tyuiu.MishaginaKS.Sprint1.Task0.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MishaginaKS.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.MishaginaKS.Sprint1.Task0.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #0 | Выполнила: Мишагина К. С. | АСОиУБ-26-1";
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                       *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #0                                                             *");
+            Console.WriteLine("* Выполнила: Мишагина Ксения Сергеевна | АСОиУБ-26-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (1+2)*(1+9/3)          *");
+            Console.WriteLine("* и печатает результат на экране.                                        *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* (1+2)*(1+9/3)                                                          *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
